f68abf8261e6e0d6bb524bada6fdd2ee1caea05f
MKowalski8 <makow.088@gmail.com>
2023-12-20 19:01:28 +0100

    Changes

--- DarwinWorld/src/main/java/WorldElements/AbstractAnimal.java
+++ DarwinWorld/src/main/java/WorldElements/AbstractAnimal.java
@@ -2,7 +2,7 @@ package WorldElements;
 
 import Components.Vector2d;
 
-public class AbstractAnimal implements WorldElement{
+public class AbstractAnimal implements Animal{
     @Override
     public Vector2d getPosition() {
         return null;
---
+++ DarwinWorld/src/main/java/WorldElements/Animal.java
@@ -0,0 +1,4 @@
+package WorldElements;
+
+public interface Animal extends WorldElement{
+}
