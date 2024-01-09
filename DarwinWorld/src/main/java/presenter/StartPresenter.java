ef03cc7481c9ee4ec2cd2a3ccacda089b8e219fc
MKowalski8 <makow.088@gmail.com>
2024-01-09 17:04:50 +0100

    Create configuartion window

--- DarwinWorld/src/main/java/presenter/StartPresenter.java
+++ DarwinWorld/src/main/java/presenter/StartPresenter.java
@@ -1,5 +1,51 @@
 package presenter;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Spinner;
+
+import java.io.IOException;
+
 public class StartPresenter {
+    @FXML
+    ComboBox mapVariant;
+
+    @FXML
+    ComboBox evolutionVariant;
+
+    @FXML
+    Spinner<Integer> mapHeight;
+
+    @FXML
+    Spinner<Integer> mapWidth;
+
+    @FXML
+    Spinner<Integer> plantNumber;
+
+    @FXML
+    Spinner<Integer> energyFromPlant;
+
+    @FXML
+    Spinner<Integer> startAnimalNumber;
+
+    @FXML
+    Spinner<Integer> startAnimalEnergy;
+
+    @FXML
+    Spinner<Integer> energyForReproduction;
+
+    @FXML
+    Spinner<Integer> maxMutationNumber;
+    @FXML
+    Spinner<Integer> minMutationNumber;
+
+    @FXML
+    Spinner<Integer> genomeLength;
+
+
+    @FXML
+    public void onSimulationStartClicked() throws IOException {
+
+    }
 }
 
