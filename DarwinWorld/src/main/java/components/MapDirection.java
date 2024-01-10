04950e34299b6e2fa1696cd6e9b42d5030eec7e0
SzymonWojturski <szykemcos137@gmail.com>
2024-01-10 19:31:14 +0100

    direction update

--- DarwinWorld/src/main/java/components/MapDirection.java
+++ DarwinWorld/src/main/java/components/MapDirection.java
@@ -6,7 +6,21 @@ public enum MapDirection {
     EAST,
     SOUTH_EAST,
     SOUTH,
+    SOUTH_WEST,
     WEST,
-    NORTH_WEST,
-    SOUTH_WEST
+    NORTH_WEST;
+
+
+    public Vector2d toVector2d() {
+        return switch (this){
+            case NORTH->       new Vector2d(0,1);
+            case NORTH_EAST -> new Vector2d(1,1);
+            case EAST->        new Vector2d(1,0);
+            case SOUTH_EAST -> new Vector2d(1,-1);
+            case SOUTH->       new Vector2d(0,-1);
+            case SOUTH_WEST -> new Vector2d(-1,-1);
+            case WEST->        new Vector2d(-1,0);
+            case NORTH_WEST -> new Vector2d(-1,1);
+        };
+    }
 }
