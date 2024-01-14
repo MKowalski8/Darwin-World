682f438c25ff0336ab573f66953a3df82f963c65
MKowalski8 <makow.088@gmail.com>
2024-01-14 02:25:19 +0100

    MapCell BugFix

--- DarwinWorld/src/main/java/maps/MapCell.java
+++ DarwinWorld/src/main/java/maps/MapCell.java
@@ -1,15 +1,13 @@
 package maps;
 
 import worldElements.Animal;
-import worldElements.Grass;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class MapCell {
 
-    private final List<Animal> animals = new ArrayList<>;
-    private final List<Animal> grasses = new ArrayList<>;
+    private final List<Animal> animals = new ArrayList<>();
+    private final List<Animal> grasses = new ArrayList<>();
 
     public void placeAnimalOnCell(Animal animal){
 
