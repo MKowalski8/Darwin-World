b7239ed3b15b0025f7930deeb0d69dd905411bfb
MKowalski8 <makow.088@gmail.com>
2024-01-16 03:05:39 +0100

    Main simulation functions

--- DarwinWorld/src/main/java/maps/MapCell.java
+++ DarwinWorld/src/main/java/maps/MapCell.java
@@ -1,5 +1,6 @@
 package maps;
 
+import components.Vector2d;
 import worldElements.Animal;
 import java.util.ArrayList;
 import java.util.List;
@@ -7,8 +8,64 @@ import java.util.List;
 public class MapCell {
 
     private final List<Animal> animals = new ArrayList<>();
+    private final List<Animal> newMovedAnimals = new ArrayList<>();
+
+    private final Vector2d cellPosition;
+
+    public MapCell(Vector2d cellPosition){
+        this.cellPosition = cellPosition;
+    }
+
 
     public void placeAnimalOnCell(Animal animal){
         animals.add(animal);
+    }
+
+    public void removeDeads(){
+        animals.forEach(animal -> {
+//            if (animal.getEnergy() <= 0){
+//                animals.remove(animal);
+//            }
+        });
+    }
+
+    public void consumePlantOnCell(int plantEnergy){
+//        animals.get(plantForAnimal()).eatPlant();
+    }
+
+    private int plantForAnimal(){
+//          TODO
+//        Tutaj bedziemy wybierali animala, ktory ma pierwszenstwo do jedzenia
+        return 0;
+    }
+
+    public void initializeReproduction(){
+        Animal animal1 = animalForReproduction();
+    }
+
+    private Animal animalForReproduction(){
+//        TODO
+        return null;
+    }
+
+    public Animal takeAnimalFromCell(){
+        return animals.remove(0);
+    }
+
+    public int animalNumber(){
+        return animals.size();
+    }
+
+    public void addMovedAnimal(Animal animal){
+        newMovedAnimals.add(animal);
+    }
+
+    public void mergeAnimals(){
+        animals.addAll(newMovedAnimals);
+        newMovedAnimals.clear();
+    }
+
+    public Vector2d getCellPosition() {
+        return cellPosition;
     }
 }
